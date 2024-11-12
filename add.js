fae8406dc8d878e16279dd6c7d6325536bbf68bb
santosh2223 <santoshkumar015@gmail.com>
2024-11-12 15:35:50 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,12 @@
+function add(a,b){ 
+return  a+b
+}
+
+function square(a){
+    return a*a
+}
+function hello(){
+    return 'Hello World';
+}
+module.exports={add,square,hello}
+
---
+++ useadd.js
@@ -0,0 +1,4 @@
+const result = require('./add');
+console.log(result.square(10));
+console.log(result.add(10,10));
+console.log(result.hello());
